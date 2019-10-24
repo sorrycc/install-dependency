47ed7dcbad51a20da232d65345a7e956e0cda70d
chencheng <sorrycc@gmail.com>
2019-10-24 16:39:35 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# install-dependency
+
+Install dependency with any npm clients.
+
+[![NPM version](https://img.shields.io/npm/v/install-dependency.svg?style=flat)](https://npmjs.org/package/install-dependency)
+[![Build Status](https://img.shields.io/travis/sorrycc/install-dependency.svg?style=flat)](https://travis-ci.org/sorrycc/install-dependency)
+[![NPM downloads](http://img.shields.io/npm/dm/install-dependency.svg?style=flat)](https://npmjs.org/package/install-dependency)
+
+## Install
+
+```bash
+$ yarn add install-dependency
+```
+
+## Usage
+
+```js
+const installDependency = require('install-dependency');
+await installDependency(pkg, cwd, opts);
+```
+
+## Options
+
+### npmClient
+
+### slient
+
+### saveDev
+
+### onData
+
+## LICENSE
+
+MIT
+
---
+++ index.js
@@ -0,0 +1,64 @@
+const assert = require('assert');
+const spawn = require('cross-spawn');
+
+const validNpmClients = [
+  'yarn',
+  'tyarn',
+  'ayarn',
+  'npm',
+  'cnpm',
+  'tnpm',
+  'pnpm',
+];
+
+function install(packageName, cwd, opts = {}) {
+  const { saveDev, slient, npmClient, onData } = opts;
+  assert(
+    npmClient,
+    `opts.npmClient must be supplied.`,
+  );
+  assert(
+    validNpmClients.includes(npmClient),
+    `Invalid opts.npmClient ${npmClient}.`,
+  );
+  const isYarn = ['yarn', 'tyarn', 'ayarn'].includes(npmClient);
+
+  let args = isYarn ? ['add'] : ['install'];
+  args.push(packageName);
+  if (saveDev) {
+    args.push(isYarn ? '-D' : '--save-dev');
+  } else if (!isYarn) {
+    args.push('--save');
+  }
+
+  return new Promise((resolve, reject) => {
+    const extraEnv = {};
+    const child = spawn(npmClient, args, {
+      cwd,
+      env: {
+        ...process.env,
+        ...extraEnv,
+      },
+    });
+    child.stdout.on('data', buffer => {
+      if (!slient) process.stdout.write(buffer);
+      if (onData) onData(buffer.toString());
+    });
+    child.stderr.on('data', buffer => {
+      if (!slient) process.stderr.write(buffer);
+      if (onData) onData(buffer.toString());
+    });
+    child.on('close', code => {
+      if (code !== 0) {
+        reject(new Error(`command failed: ${npmClient} ${args.join(' ')}`));
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+module.exports = async function (...args) {
+  // TODO: Promise ╤сап
+  await install(...args);
+}
